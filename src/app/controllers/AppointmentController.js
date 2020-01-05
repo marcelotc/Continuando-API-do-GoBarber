c6cdf3a1ae18aa4663fe73bb464af24d4e0bccf6
Marcelo Cortes <marcelocortes@pop-os.localdomain>
2020-01-04 22:09:38 -0300

    Validações de agendamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -2,6 +2,8 @@ import Appointment from '../models/Appointment';
 import User from '../models/User';
 import * as Yup from 'yup';
 
+import { startOfHour, parseISO, isBefore } from 'date-fns';
+
 class AppointmentController {
     async store(req, res) {
         const schema = Yup.object().shape({
@@ -27,10 +29,36 @@ class AppointmentController {
             return res.status(401).json({ error: 'You can only create appointments with providers.' });
         }
 
+        /**
+         * check for past dates
+         */
+
+        const hourStart = startOfHour(parseISO(date));
+
+        if (isBefore(hourStart, new Date())) {
+            return res.status(400).json({ error: 'Paste dates are not permitted.' })
+        }
+
+        /**
+         * check date availability
+         */
+
+        const checkAvailability = await Appointment.findOne({
+            where: {
+                provider_id,
+                canceled_at: null,
+                date: hourStart
+            }
+        });
+
+        if (checkAvailability) {
+            return res.status(400).json({ error: 'Appointment date is not available.' })
+        }
+
         const appointment = await Appointment.create({
             user_id: req.userId,
             provider_id,
-            date
+            date: hourStart
         });
 
         return res.json(appointment);
