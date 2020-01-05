588eae9f70ebef9ff01840d74bc43806074cec33
Marcelo Cortes <marcelocortes@pop-os.localdomain>
2020-01-05 15:02:58 -0300

    Cancelamento de agendamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -4,7 +4,7 @@ import File from '../models/File';
 import * as Yup from 'yup';
 import Notification from '../schemas/Notification';
 
-import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 
 class AppointmentController {
@@ -114,6 +114,34 @@ class AppointmentController {
             content: `Novo agendamento de ${user.name} para ${formattedDate}`,
             user: provider_id
         });
+
+        return res.json(appointment);
+    }
+
+    async delete(req, res) {
+        const appointment = await Appointment.findByPk(req.params.id);
+
+        if (appointment.user_id !== req.userId) {
+            return res.json(401).json({
+                error: "You don't have permission to cancel this appointment."
+            });
+        }
+
+        const dateWithSub = subHours(appointment.date, 2);
+
+        //appointment.date: 13:00 
+        //dateWithSub 11h
+        //now: Horário no computador
+
+        if(isBefore(dateWithSub, new Date())){
+            return res.status(401).json({
+                error: 'You can only cancel appointments 2 hours in advance.'
+            })
+        }
+
+        appointment.canceled_at = new Date();
+
+        await appointment.save();
 
         return res.json(appointment);
     }
