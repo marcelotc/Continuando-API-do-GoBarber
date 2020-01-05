5ccf4e377accc6ecdce6be323fd906e6c7154942
Marcelo Cortes <marcelocortes@pop-os.localdomain>
2020-01-05 01:12:31 -0300

    Notificando novos agendamentos

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -2,8 +2,10 @@ import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 import * as Yup from 'yup';
+import Notification from '../schemas/Notification';
 
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
 class AppointmentController {
     async index(req, res) {
@@ -88,6 +90,21 @@ class AppointmentController {
             user_id: req.userId,
             provider_id,
             date: hourStart
+        });
+
+        /**
+         * Notifify appoitment provider
+         */
+        const user = await User.findByPk(req.userId);
+        const formattedDate = format(
+            hourStart,
+            "'dia' dd 'de' MMMM', às ' H:mm'h'",
+            { locale: pt }
+        );
+
+        await Notification.create({
+            content: `Novo agendamento de ${user.name} para ${formattedDate}`,
+            user: provider_id
         });
 
         return res.json(appointment);
